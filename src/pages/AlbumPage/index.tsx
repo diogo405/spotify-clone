855b9015fd9ad774d7aba17fa186288f59d5cf76
Diogo Goncalves <goncalvesdiogo99@gmail.com>
2021-05-03 21:05:45 +1000

    feat: album page

--- src/pages/AlbumPage/index.tsx
+++ src/pages/AlbumPage/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import Back from '../../components/Back'
+import Play from '../../components/Play'
+import Heart from '../../components/Heart'
+import DotMenu from '../../components/DotMenu'
 import './styles.css'
 
 const AlbumPage = () => {
   return (
     <>
       <Back url="/" />
+      <header className="header">
+        <div className="header__image" style={{backgroundImage: 'url(https://i.scdn.co/image/ab67616d0000b273f907de96b9a4fbc04accc0d5)'}}></div>
+        <div className="header__wrapper">
+          <h2 className="header__subtitle">album</h2>
+          <h1 className="header__title">Scorpion</h1>
+        </div>
+      </header>
+      <div className="actions">
+        <Play size="big" />
+        <Heart size="big" />
+        <DotMenu />
+      </div>
     </>
   )
 }
