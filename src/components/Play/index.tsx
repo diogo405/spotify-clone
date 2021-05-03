86bb3085280345ee08f4e77d6240faf885951244
Diogo Goncalves <goncalvesdiogo99@gmail.com>
2021-05-03 21:05:30 +1000

    style: play

--- src/components/Play/index.tsx
+++ src/components/Play/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import './styles.css'
 
-const Play = () => {
+interface IPlay {
+  size?: 'regular' | 'big'
+}
+
+const Play: React.FC<IPlay> = ({size = 'regular'}) => {
+
   return (
-    <button className="play">
+    <button className={size === 'big' ? 'play play--big' : 'play'}>
       <svg
         className="play__icon"
         xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
         viewBox="0 0 24 24"
       >
         <path d="M3 22v-20l18 10-18 10z" />
