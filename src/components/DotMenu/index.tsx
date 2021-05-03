012cfd9954e19eaef65e16def94d5b5649275551
Diogo Goncalves <goncalvesdiogo99@gmail.com>
2021-05-03 21:04:33 +1000

    feat: dot menu

---
+++ src/components/DotMenu/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles.css'
+
+const DotMenu = () => {
+  return (
+    <div className="dotmenu">
+      <div/>
+      <div/>
+      <div/>
+    </div>
+  )
+}
+
+export default DotMenu
